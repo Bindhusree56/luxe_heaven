ba23549f89aed50e65a9f06f1a22741739723f90
Bindhu <bindhusree564@gmail.com>
2025-12-26 11:02:21 +0530

    debbuging register issue

--- backend/models/Cart.js
+++ backend/models/Cart.js
@@ -29,9 +29,8 @@ const cartSchema = new mongoose.Schema({
 });
 
 // Update the updatedAt field before saving
-cartSchema.pre('save', function (next) {
+cartSchema.pre('save', function () {
   this.updatedAt = Date.now();
-  next();
 });
 
 module.exports = mongoose.model('Cart', cartSchema);
